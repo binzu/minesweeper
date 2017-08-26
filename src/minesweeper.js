cc0d629ade46d7245262afc0ed3a26d3fd44154f
Vince Golveo <vince.golveo@gmail.com>
2017-08-26 16:15:31 -0400

    create empty game board

---
+++ src/minesweeper.js
@@ -0,0 +1,11 @@
+const blankLine = '  |   |  ';
+const guessLIne = '1 |   |  ';
+const bombLine = '  | B |  ';
+console.log(`This is what an empty board would look like:`);
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+console.log(`This is what a board with a guess and a bomb on it would look like:`);
+console.log(guessLIne);
+console.log(bombLine);
+console.log(blankLine);
